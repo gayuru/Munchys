17b843bd291419fdaf944e7673bf81e9797ddbd9
Gayuru <ggayurugunawardana@gmail.com>
2020-04-15 23:16:05 +1000

    Landing page design

--- react/src/Pages/Dashboard.js
+++ react/src/Pages/Dashboard.js
@@ -5,17 +5,55 @@ import React, {useEffect,useState} from 'react';
 
 //Plugins
 import styled from 'styled-components';
-import Container from 'react-bootstrap/Container'
+import {Container,Row,Image, Col} from 'react-bootstrap';
+import logo from '../media/logo-coloured.svg'
+
 //Component Imports
 
 //////////////////////////////
 //Styled components
 const FullHeightContainer = styled(Container)`
-  height:100vh;
-  border-style: solid;
-  border-color: #36FFC1;
+  // height:100vh;
+  // border-style: solid;
+  // border-color: #36FFC1;
+`
+const Logo = styled(Image)`
+  margin-top:3vh;
+`
+const MainText = styled.text`
+font-size: 5em;
+margin-left:5vw;
+text-shadow: 1px 4px 8px rgba(0, 0, 0, 0.28);
+display:block;
+`
+const SubText = styled.span`
+font-style: italic;
+margin-left:20vw;
+font-weight: bold;
+font-size: 8em;
+line-height: 156px;
+/* identical to box height */
+color: #7F95D1;
+display:block;
 `
 
+const CustomRow = styled(Row)`
+  margin-top:${props => props.top};
+`
+
+const TextBox = styled.div`
+  margin-left:5vw;
+  border-left:3px solid #7F95D1;
+  padding:10px 10px 10px 15px;
+`
+const Subline = styled.p`
+line-height: 23px;
+text-align: justify;
+`
+const Blue = styled.span`
+font-weight: bold;
+color: #7F95D1;
+`
 //////////////////////////////
 //Dashboard class
 /**
@@ -24,31 +62,31 @@ const FullHeightContainer = styled(Container)`
 function Dashboard(props) {
   const [currentTime,setCurrentTime] = useState(0);
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data=>{
-      setCurrentTime(data.time)
-    });
-  },[]);
-
-  function convert(str) {
-    var date = new Date(str),
-      mnth = ("0" + (date.getMonth() + 1)).slice(-2),
-      day = ("0" + date.getDate()).slice(-2);
-    return [date.getFullYear(), mnth, day].join("-");
-  }
-
-  const exampleFunction = () => {
-    return (
-      <h1>
-        The current time is {convert(currentTime)}
-      </h1>
-    )
-  }
 
   return (
-  <FullHeightContainer>
-    {exampleFunction()}
-  </FullHeightContainer>
+  <Container>
+    <Row>
+    <Logo src={logo}/>
+    </Row>
+    <CustomRow top="10vh">
+    <MainText>
+    Every Day is <br/>
+    </MainText>
+    <SubText>
+    Tastyyyy
+    </SubText>
+    </CustomRow>
+    <CustomRow top="15vh">
+    <TextBox>
+      <Subline>
+      Lorem ipsum dolor sit amet, <Blue>consectetur</Blue> adipiscing<br/>
+       elit, sed do eiusmod tempor incididunt ut labore et<br/> 
+       dolore magna aliqua. <Blue>Ut enim</Blue> ad minim veniam,<br/>
+        quis nostrud exercitation ullamco laboris <Blue>nisi ut.</Blue>
+      </Subline>
+    </TextBox>
+    </CustomRow>
+  </Container>
   )
 }
 export default Dashboard;
