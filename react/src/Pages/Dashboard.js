907f9c651b90bdf77e2c9687c6ce92dd78355276
Gayuru <ggayurugunawardana@gmail.com>
2020-04-15 23:55:42 +1000

    landing page animation

--- react/src/Pages/Dashboard.js
+++ react/src/Pages/Dashboard.js
@@ -7,16 +7,13 @@ import React, {useEffect,useState} from 'react';
 import styled from 'styled-components';
 import {Container,Row,Image, Col} from 'react-bootstrap';
 import logo from '../media/logo-coloured.svg'
-
+import backgroundImage from '../media/background.jpg'
+import Typing from 'react-typing-animation';
 //Component Imports
 
 //////////////////////////////
 //Styled components
-const FullHeightContainer = styled(Container)`
-  // height:100vh;
-  // border-style: solid;
-  // border-color: #36FFC1;
-`
+
 const Logo = styled(Image)`
   margin-top:3vh;
 `
@@ -28,13 +25,14 @@ display:block;
 `
 const SubText = styled.span`
 font-style: italic;
-margin-left:20vw;
+margin-left:25vw;
 font-weight: bold;
 font-size: 8em;
 line-height: 156px;
 /* identical to box height */
 color: #7F95D1;
 display:block;
+z-index:1;
 `
 
 const CustomRow = styled(Row)`
@@ -54,6 +52,19 @@ const Blue = styled.span`
 font-weight: bold;
 color: #7F95D1;
 `
+const Graphic = styled.div`
+position: absolute;
+width: 606px;
+height: 1012px;
+    top: 0px;
+    right: 0px;
+z-index:-1;
+background: url(${backgroundImage})100% no-repeat;
+background-size: cover;
+    background-position: center; 
+box-shadow: 1px -7px 53px rgba(0, 0, 0, 0.1);
+border-radius: 0px 0px 0px 141px;
+`
 //////////////////////////////
 //Dashboard class
 /**
@@ -62,28 +73,36 @@ color: #7F95D1;
 function Dashboard(props) {
   const [currentTime,setCurrentTime] = useState(0);
 
+  useEffect(() => { 
+    document.body.style.backgroundColor = '#FFF5F5'
+   }, []) 
 
   return (
   <Container>
     <Row>
     <Logo src={logo}/>
+    <Graphic/>
     </Row>
     <CustomRow top="10vh">
+   
     <MainText>
     Every Day is <br/>
     </MainText>
     <SubText>
     Tastyyyy
     </SubText>
+
     </CustomRow>
     <CustomRow top="15vh">
     <TextBox>
+    <Typing speed={50}>
       <Subline>
       Lorem ipsum dolor sit amet, <Blue>consectetur</Blue> adipiscing<br/>
        elit, sed do eiusmod tempor incididunt ut labore et<br/> 
        dolore magna aliqua. <Blue>Ut enim</Blue> ad minim veniam,<br/>
         quis nostrud exercitation ullamco laboris <Blue>nisi ut.</Blue>
       </Subline>
+      </Typing>
     </TextBox>
     </CustomRow>
   </Container>
