6d8a51f9afcb016355569fc9b2f5196e89e49fb8
Gayuru <ggayurugunawardana@gmail.com>
2020-04-24 01:02:45 +1000

    Recipe Page done

--- react/src/Pages/RecipePage.js
+++ react/src/Pages/RecipePage.js
@@ -8,8 +8,9 @@ import styled from 'styled-components';
 import Banner from '../media/banner.svg'
 import logo from '../media/logo-coloured.svg'
 import heart from '../media/heart.svg'
-//Component Imports
 
+//Component Imports
+import Recipe from '../Components/SingleRecipe'
 //////////////////////////////
 //Styled components
 const Logo = styled(Image)`
@@ -27,14 +28,26 @@ margin-top:2vh;
 `
 const BadgeCustom = styled(Badge)`
 padding: 1vw 3vw 1vw 3vw;
-// padding-left:3vw;
-// padding-right:3vw;
 margin-left:2vw;
 background: #FFC0BE;
 color: #000000;
 font-size: 20px;
 font-style: normal;
 font-weight: normal;
+`
+const RecipeHeading = styled.h2`
+margin-top:2vh;
+font-style: normal;
+font-weight: bold;
+font-size: 43px;
+line-height: 65px;
+letter-spacing: -0.025em;
+color: #7F95D1;
+`
+
+const CustomRow = styled(Row)`
+margin-top:3vh;
+height:100vh;
 `
 
 //////////////////////////////
@@ -73,9 +86,20 @@ function RecipePage(props) {
     <Row>
     <IngredientText>
         Ingredients Chosen {renderIngredients()}
-      </IngredientText>
-     
+    </IngredientText>
     </Row>
+    <Row>
+      <RecipeHeading>
+      Recipes generated
+      </RecipeHeading>
+    </Row>
+    <CustomRow>
+      <Col>
+      <Recipe/></Col>
+     
+    
+    </CustomRow>
+    
   </Container>
   )
 }
