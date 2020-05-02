83cd3261372ebce6e050aa869c85b059ed4dca47
Gayuru <ggayurugunawardana@gmail.com>
2020-05-02 16:35:58 +1000

    Top part done

--- react/src/Pages/Recipe.js
+++ react/src/Pages/Recipe.js
@@ -5,11 +5,11 @@ import React from 'react';
 
 //Plugins
 import styled from 'styled-components';
-import { Badge, Col, Container, Image, Row } from 'react-bootstrap';
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
 //Plugins
 import Banner from '../media/banner.svg';
 import heart from '../media/heart.svg';
-import heartUnlike from '../media/heart-unliked.svg';
+import Clock from '../media/clock-colored.svg';
 import logo from '../media/logo-coloured.svg';
 import GridGenerator from '../Components/GridGenerator';
 import Back from '../media/back.svg';
@@ -39,7 +39,57 @@ border-radius: 18px;
 `
 const HeadingSection = styled(Row)`
   margin-left:1vw;
-  margin-top:2vh;
+  margin-top:1vh;
+`
+const TimeText = styled.text`
+font-weight: bold;
+font-size: 20px;
+letter-spacing: -0.025em;
+color: #7F95D1;
+margin-left:1vw;
+margin-bottom:0.5vh;
+`
+const HeadingText = styled.text`
+font-weight: bold;
+font-size: 51px;
+line-height: 68px;
+// letter-spacing: -0.025em;
+text-align:left;
+color: #7F95D1;
+`
+const BodyText = styled.text`
+font-weight: normal;
+font-size: 16px;
+line-height: 21px;
+text-align: justify;
+margin-top:1vh;
+`
+
+
+const CustomButton = styled(Button)`
+border: 2px solid #F7AF9D;
+box-sizing: border-box;
+box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.13);
+margin-top:1vh;
+background-color: Transparent;
+background-repeat:no-repeat;
+color: #000000;
+
+&:hover,&:focus,&:active{
+  color: white !important;
+  background-color: #7F95D1; !important;
+  border-color: #7F95D1; !important;
+}
+`
+
+const FactText = styled.text`
+font-weight: bold;
+font-size: 31px;
+line-height: 68px;
+// letter-spacing: -0.025em;
+text-align:left;
+color: #7F95D1;
+margin-top:2vh;
 `
 
 //////////////////////////////
@@ -52,6 +102,10 @@ function Recipe(props) {
   const recipeId = props.match.params.id
   let history = useHistory();
 
+  const handleClick = () => {
+    console.log("make this recipe favourite")
+  }
+
   const RenderTop = ()=>{
     return(
       <>
@@ -60,14 +114,61 @@ function Recipe(props) {
       </Col>
       <Col>
       <HeadingSection>
-      
-        hi
-      
+        <Image src={Clock}/> 
+        <TimeText>
+          30min
+        </TimeText>
+        <TimeText>
+          |
+        </TimeText>
+        <TimeText>
+          by John Doe
+        </TimeText>
       </HeadingSection>
-     
-      
+      <HeadingSection>
+        <HeadingText>
+        Pineapple & Cucumber
+        Salad
+        </HeadingText>
+      </HeadingSection>
+      <HeadingSection>
+        <BodyText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+          ut labore et dolore magna aliqua. Mauris pellentesque pulvinar pellentesque habitant morbi tristique. 
+          Augue mauris augue neque gravida in fermentum. <br/><br/>
+
+          Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Pellentesque massa placerat duis ultricies lacus sed. Tincidunt tortor aliquam nulla facilisi cras.
+        </BodyText>
+      </HeadingSection>
+      <HeadingSection>
+      <CustomButton onClick={handleClick}>
+        Favourite this ❤️
+      </CustomButton>
+      </HeadingSection>
       </Col>
       </>
+    )
+  }
+
+  function Fact(props){
+    return(
+      <Col>
+      <Row>
+        <Image src={props.image}/>
+        {props.fact}
+      </Row>
+      <Row>
+        {props.name}
+      </Row>
+      </Col>
+    )
+  }
+
+  const RenderQuickFacts =()=>{
+    return(
+      <HeadingSection>
+        <Fact image={Clock} name="Savings" fact="4"/>
+      </HeadingSection>
     )
   }
   return (
@@ -82,10 +183,15 @@ function Recipe(props) {
         </Row>
         <Row>
           <RenderTop/>
-
-
-
         </Row>
+        <Row>
+          <FactText>
+            Quick Facts
+          </FactText>
+        </Row>
+       
+          <RenderQuickFacts/>
+        
         </Container>
   )
 }
