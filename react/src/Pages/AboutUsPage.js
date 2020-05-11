6b574466aa5b5e7c821bf78168193bfd8d826f59
Gayuru <ggayurugunawardana@gmail.com>
2020-05-11 11:12:57 +1000

    Profile added

--- react/src/Pages/AboutUsPage.js
+++ react/src/Pages/AboutUsPage.js
@@ -1,7 +1,7 @@
 //Template.js
 ///////////////////////////////
 import React, { useEffect, useState } from 'react';
-import { Badge, Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row } from 'react-bootstrap';
 import { Link, useHistory } from "react-router-dom";
 //Plugins
 import styled from 'styled-components';
@@ -10,7 +10,7 @@ import Team from '../media/team.svg';
 
 import heart from '../media/heart.svg';
 import logo from '../media/logo-coloured.svg';
-
+import GayuruProfile from '../media/gayuru.jpg'
 import { trackPromise } from 'react-promise-tracker';
 //Component Imports
 
@@ -25,7 +25,29 @@ const Heart = styled(Image)`
 float:right;
 margin-top:5vh;
 `
+const ImageRow = styled(Row)`
+justify-content:center;
 
+`
+
+const Heading = styled.h1`
+margin-top:3vh;
+font-weight:bold;
+`
+const NameHeading = styled.h2`
+margin-top:1vh;
+font-weight:bold;
+`
+
+const ProfileImage = styled(Image)`
+  width:200px;
+`
+const Spacer = styled.div`
+  height: ${props => props.height};
+`
+const Text = styled.text`
+white-space: pre-wrap;
+`
 //////////////////////////////
 //Component class
 /**
@@ -35,6 +57,23 @@ function AboutUsPage(props) {
 
   const exampleFunction = () => {
     return "My example";
+  }
+
+  const MyProfile =(props)=>{
+    return(
+      <>
+      <Col>
+      <ProfileImage src={props.image} roundedCircle/>
+      <NameHeading>
+          Hey I'm {props.name}
+      </NameHeading>
+      <Text>
+        {props.intro}
+      </Text>
+      </Col>
+      </>
+
+    )
   }
 
   return (
@@ -51,8 +90,20 @@ function AboutUsPage(props) {
       </Link>
       </Col>
     </Row>
-    <Row>
+    <ImageRow>
       <Image src={Team} fluid/>
+    </ImageRow>
+    <ImageRow>
+      <Heading>'We' is powerful.</Heading>
+    </ImageRow>
+    <Spacer height="1vh"/>
+    <ImageRow>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit,  <br/>tempor incididunt ut labore et dolore magna aliqua.mauris a diam.<br/> Proin libero nunc consequat interdum varius sit amet mattis.
+    </ImageRow>
+    <Spacer height="3vh"/>
+    <Row>
+      <MyProfile name="Gayuru" image={GayuruProfile}intro={`Hey I code for fun \n this is my lad`}/>
+      <MyProfile name="Saad"/>
     </Row>
     </Container>
   )
